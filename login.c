c754004b5ddaebfb565f4f47687a2a5844a0ba87
Youri TAMITEGAMA <ytamit@trusty>
2016-04-04 17:54:22 -0400

    unstable, just testing out stuff

--- login.c
+++ login.c
@@ -5,10 +5,32 @@
 //displays the error page
 void displayError(void);
 
+//imported from https://www.cs.tut.fi/~jkorpela/forms/cgic.html
+//translates the input into a readable format (with special characters still)
+void unencode(char *src, char *last, char *dest);
+
 int main()
 {
-	char *username = "thisisausername";
+	char input[300], data[300];
+	char inputUsrname[51], inputPasswd[51];
+	char *ptr;
+	int a;
+	int n = atoi(getenv("CONTENT_LENGTH"));
+
+	fgets(input, n+1, stdin);
+	unencode(input, input+n, data);
+
+	ptr = 5+strstr(data, "user=");
+	for (a=0; *(ptr+a)!='&'; a++) {
+		inputUsrname[a] = *(ptr+a);//not sure this will work
+	}
+
+	ptr = 7+strstr(data, "passwd=");
+	for (a=0; *(ptr+a)!='&'; a++) {
+		inputPasswd[a] = *(ptr+a); //not sure this will work
+	}
 	
+	if (strcmp(
 	displayError();
 	return 0;
 }
@@ -26,3 +48,20 @@ void displayError(void)
 	printf("back to the <a href=\"index.html\">Home Page</a>\n");
 	printf("</body>\n");
 }
+
+void unencode(char *src, char *last, char *dest)
+{
+ for(; src != last; src++, dest++)
+   if(*src == '+')
+     *dest = ' ';
+   else if(*src == '%') {
+     int code;
+     if(sscanf(src+1, "%2x", &code) != 1) code = '?';
+     *dest = code;
+     src +=2; }
+   else
+     *dest = *src;
+ *dest = '\n';
+ *++dest = '\0';
+}
+
