2eaddb579c1f9b254f093de1b8e4d0e44bd8dbc0
Youri TAMITEGAMA <ytamit@linux.CS.McGill.CA>
2016-04-06 01:22:08 -0400

    Started debugging login.c

--- login.c
+++ login.c
@@ -14,28 +14,32 @@ void unencode(char *src, char *last, char *dest);
 
 int main()
 {
-	char input[300], data[300];
+	char input[500], data[500];
 	char inputUsrname[51], inputPasswd[51];
 	char *ptr;
 	int a;
 	int n = atoi(getenv("CONTENT_LENGTH"));
 
+	printf("%s%c%c\n","Content-Type:text/html;charset=iso-8859-1",13,10);
+	printf("<body>%d</body>", n);
+
 	fgets(input, n+1, stdin);
 	unencode(input, input+n, data);
+	//good
 
 	ptr = 5+strstr(data, "user=");
 	for (a=0; *(ptr+a)!='&'; a++) {
-		inputUsrname[a] = *(ptr+a);//not sure this will work
+		inputUsrname[a] = (char)*(ptr+a);//not sure this will work
 	}
 
 	ptr = 7+strstr(data, "passwd=");
 	for (a=0; *(ptr+a)!='&'; a++) {
-		inputPasswd[a] = *(ptr+a); //not sure this will work
+		inputPasswd[a] = (char)*(ptr+a); //not sure this will work
 	}
 	//at this point, we have the inputed password/username.
 
 	if (isValid(inputUsrname, inputPasswd)) {
-		setenv("usr", usr, 1);
+		setenv("usr", inputUsrname, 1);
 		system("./cgi-bin/dashboard.cgi $usr");
 	}
 	else displayError();
@@ -49,7 +53,7 @@ int isValid(char *usr, char *pwd)
 	char line[100];
 	FILE *f;
 
-	f = fopen(user.txt, "rt");
+	f = fopen("./users.txt", "rt");
 	fgets(line, 99, f);
 
 	while (line != 0) {
@@ -67,6 +71,7 @@ int isValid(char *usr, char *pwd)
 
 void displayError(void)
 {
+
 	printf("%s%c%c\n","Content-Type:text/html;charset=iso-8859-1",13,10);
 	printf("<head>\n<title>Authentification Failed</title>\n");
 	printf("<link href=\"http://cs.mcgill.ca/~ytamit/global.css\" rel=\"stylesheet\" type=\"text/css\">\n");
