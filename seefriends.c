3b2ac93554ae7cc3ccd2a76ee2175869d167a38a
dahanajoseph <dahana.joseph@mail.mcgill.ca>
2016-04-07 12:57:32 -0400

    Still need to fully get how cgi works

--- seefriends.c
+++ seefriends.c
@@ -5,6 +5,25 @@
 // ------------------------------------------------------------------------
 
 
+ 
+// there is going to be a friends.txt created by makefriends.py
+
+// This is a C program called seefriends.c .
+// This program generates a webpage that lists all the usernames of friends for the current user. 
+// Determine a mechanism by which the user can pick one friend from the list.
+// The program then displays the profile for that user.
+// The program returns to the see a friend page.
+
+
+// friends.txt is formatted in the following way: 
+
+// username friend1 friend2 friend3...
+// where each token is a username
+
+// so seefriends.c fget each line, tokenize, look for the username
+// If it is the good username, list each friend's names radio button cgi
+// submit button
+
 
 
 // ------------------------------------------------------------------------
@@ -39,7 +58,7 @@ int getFriends(char *user){
 		}
 
 		else {
-			listFriends(i);
+			listFriends(user, i);
 			fclose(file_ptr);
 			return EXIT_SUCCESS;
 		}
@@ -51,7 +70,7 @@ int getFriends(char *user){
 
 // ------------------------------------------------------------------------
 
-int listFriends(int i){
+int listFriends(char *user, int i){
 
 	FILE *file_ptr;
         char line[2048];
@@ -78,7 +97,7 @@ int listFriends(int i){
 
 	}
 
-	printf(" <input type=\"submit\" value=\" See your friend's page\" ");
+	prinf(" <input type=\"submit\" value=\" See your friend's page\" ");
 
 
 }
