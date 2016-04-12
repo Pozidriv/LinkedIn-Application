0cf4c5cf278b50d75d5c1fe90c5ee9515042199d
Youri TAMITEGAMA <ytamit@trusty>
2016-04-12 16:31:02 -0400

    Added a function to retrieve the friendlist

--- newfriends.py
+++ newfriends.py
@@ -1,39 +1,39 @@
 #!/usr/bin/python
-import urllib
+import urllib2
+import cgitb
+import cgi
+import sys
+cgitb.enable()
+
+# We assume that the method used is POST
 
 print "Content-Type:text/html\n\n"
 
 listFriends = []
-string = ""
 
-
-# This function will receive as input a string representing a URL.
-# It will output the get request passed in that URL
-def fetchGetData(url) :
-	getData = ""
-	for i in range(len(url)) :
-		if url[i] == "?" :
-			getData = url[i:len(url)-2]
-	return getData
-
-# This function will receive as input a string in get format and will output a dictionnary 
-# associating each variable name to its value.
-def decode(string) :
-	for i in range(len(string)) :
-		if string[i] == "+" :
-			string[i] = " "
-		elif string[i] == "%" :
-			return
-	return
+arguments = cgi.FieldStorage()
+# This function will receive as input arguments (as returned by cgi.FieldStorage()
+# It will return the list of friends to add to the list
+# Tested, works
+def getFriends(arguments) :
+	for name in arguments :
+		listFriends.append(name)
+	return listFriends
 
 # This function will receive as input a list of names, and a string representing a username. 
 # It will add each friend of the list to the friends of the given user.
 def addFriends(listFriends, username) :
 	return
-	
 
+print "<html>"
 
-url = urllib.request.full_url
+# url = urllib2.Request.get_full_url()
 
-data = fetchGetData(url)
-print url
+# data = fetchGetData(url)
+# print url
+
+listFriends = getFriends(arguments)
+
+print listFriends
+
+print "</html>"
