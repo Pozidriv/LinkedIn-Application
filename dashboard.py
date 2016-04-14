63ac2b69f680633af2b05fc4168f3ecff7a371ed
Samuel GREGOIRE <samuelg207@gmail.com>
2016-04-14 15:19:43 -0400

    stuff to commit blabla

--- dashboard.py
+++ dashboard.py
@@ -1,1 +1,14 @@
+import sys
+import re
+
+username = sys.argv[1]
+friends = open("friends.txt","r")
+
+"""Acquires the list of friends of user"""
+for line in friends:
+	if re.match(username,line):
+		freindList = line.split()
+friends.close()
+
+status = open("status.txt","r")
 
