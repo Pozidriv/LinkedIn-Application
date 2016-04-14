92ee3a126a8048951e7520c8d686390bf90f4ffa
Samuel GREGOIRE <samuelg207@gmail.com>
2016-04-14 15:46:37 -0400

    Updated to work with cgi and custom libs

--- register.c
+++ register.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <strings.h>
+#include "decd_post.h"
 
 void displayError1() //Error page when fields left blank
 {
@@ -48,6 +49,9 @@ int usernameTaken(char *username)
 			fclose(fp);
 			return 1;
 		}
+		fgets(nextLine, 5000, fp);
+		fgets(nextLine, 5000, fp);
+		fgets(nextLine, 5000, fp);
 	}
 	fclose(fp);
 	return 0;
@@ -55,13 +59,22 @@ int usernameTaken(char *username)
 
 int main(int argc, char *argv[])
 {
-	if(argc != 5)
+	char input[5000], data[5000];
+	char username[5000], psswd[5000], jbdsc[5000], fullnm[5000];
+	fgets(input, n+1, stdin);
+	unencode(input, input + n, data);
+	getVariable(data, "user", username);
+	getVariable(data, "passswd", psswd);
+	getVariable(data, "fullname", fullnm);
+	getVariable(data, "jobdesc", jbdsc);
+	
+	if(username == NULL || psswd == NULL || fullnm == NULL || jbdsc == NULL)
 	{
 		displayError1();
 		return 1;
 	}
 
-	if(usernameTaken(argv[1]))
+	if(usernameTaken(username))
 	{
 		displayError2();
 		return 2;
@@ -70,7 +83,7 @@ int main(int argc, char *argv[])
 	FILE *fp;
 	fp = fopen("users.txt", "a");
 	
-	fprintf(fp, "%s\n%s\n%s\n%s\n", argv[1], argv[2], argv[3], argv[4]);
+	fprintf(fp, "%s\n%s\n%s\n%s\n", username, psswd, fullnm, jbdsc);
 	fclose(fp);
 	displaySuccess();
 	return 0;
