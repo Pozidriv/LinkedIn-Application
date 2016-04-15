8358f6e7c6993e44bdb39a35165831522af7455f
Samuel GREGOIRE <samuelg207@gmail.com>
2016-04-15 09:55:49 -0400

    Pretty much final, apart from style. register.c still throws the server error though...

--- dashboard.py
+++ dashboard.py
@@ -1,36 +1,62 @@
 #!/usr/bin/python
+import sys
+import os
 import cgi
 import cgitb
 cgitb.enable()
 
-"""Method that returns the 20 most recent statuses made by friends in html format"""
+print "Content-Type: text/html"
+print
+
+#Method that returns the 20 most recent statuses made by friends in html format
 def displayStatuses(friends):
-	numberOfStatuses = 0
+	try:
+		statusFile = open("status.txt","r")
+	except IOError:
+		print "Didn't work"
+		sys.exit()
+
+	i = 0
 	statuses = "<p>"
-	status = open("status.txt","r")
-	for line in reversed(status.readlines()):
+	chronoStatus = reversed(statusFile.readlines())
+	for line in chronoStatus:
 		splittedStatus = line.split(' ', 1)
 		if splittedStatus[0] in friends and i < 20:
 			statuses += "<br />" + splittedStatus[0].replace("$", "") + " said:<br /> " + splittedStatus[1]
-			numberOfStatuses++
+			i+=1
 	statuses += "</p>"
+	statusFile.close()
 	return statuses
 
-argv = cgi.FieldStorage()
-username = argv.getvalue("name")
-friends = open("friends.txt","r")
+arguments = cgi.FieldStorage()
+username = arguments.getvalue("username")
+friendList = []
 
-"""Acquires the list of friends of user"""
+try:
+	friendFile = open("friends.txt","r")
+except IOError:
+	print "Friends no work"
+	sys.exit()
+
+friends = friendFile.readlines()
+
+#Acquires the list of friends of user
 for line in friends:
 	if line.find("$"+username):
-		friendList = line.split(",") """Will find way to separate username, come back when idea happens"""
-friends.close()
-['$' + i for i in friendList] """adds '$' to the beginning of all names"""
+		newLine = line.replace(" ", ",", 1)
+		newLine = newLine.replace("$", "")
+		friendList += newLine.split(",")
+friendFile.close()
 
-"""This is the part that displays the page"""
-dashFile = open("dashboard.html","r")
+#This is the part that displays the page
+try:
+	dashFile = open("dashboard.html","r")
+except IOError:
+	print "no dash"
+	sys.exit()
+
 dash = dashFile.readlines()
 for line in dash:
-	line = line.replace("Name here", username)
-	line = line.replace("Statuses here", displayStatuses(friendList))
-	print line
+	newLine = line.replace("Name here", username)
+	newLine = newLine.replace("Statuses here", displayStatuses(friendList))
+	print newLine
--- register.c
+++ register.c
@@ -61,10 +61,12 @@ int usernameTaken(char *username) //checks if username is taken
 int main(int argc, char *argv[])
 {
 	//make the inputs readable
-	char input[5000], data[5000];
+/*	char input[5000], data[5000];
 	char username[5000], psswd[5000], jbdsc[5000], fullnm[5000];
 	int n;
 	
+	printf("%s%c%c\n","Content-Type:text/html;charset=iso-8859-1",13,10);
+
 	if (getenv("CONTENT_LENGTH") != NULL)
 		n = atoi(getenv("CONTENT_LENGTH"));
 	else
@@ -76,10 +78,14 @@ int main(int argc, char *argv[])
 	fgets(input, n+1, stdin);
 	unencode(input, input + n, data);
 	getVariable(data, "user", username);
-	getVariable(data, "passswd", psswd);
+	getVariable(data, "passwd", psswd);
 	getVariable(data, "fullname", fullnm);
 	getVariable(data, "jobdesc", jbdsc);
-	
+*/
+	char *username = "test";
+	char * psswd = "test";
+	char *fullnm = "test";
+	char *jbdsc = "test";
 	//checks if any field left blank
 	if(username == NULL || psswd == NULL || fullnm == NULL || jbdsc == NULL)
 	{
